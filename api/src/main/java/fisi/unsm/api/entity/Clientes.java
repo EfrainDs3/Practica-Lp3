6d85bc8321f4e614d82f2979347aa6a01741309a
EfrainDs3 <santiagotorresmurrieta@gmail.com>
2025-09-10 10:18:00 -0500

    api

---
+++ api/src/main/java/fisi/unsm/api/entity/Clientes.java
@@ -0,0 +1,83 @@
+package fisi.unsm.api.entity;
+
+
+
+public class Clientes {
+    
+    private Integer id;
+    private String nombre;
+    private String correo;
+    private String zip;
+    private String telefono1;
+    private String telefono2;
+    private String pais;
+    private String direccion;
+    private Integer estado = 1;
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    public String getTelefono1() {
+        return telefono1;
+    }
+    public void setTelefono1(String telefono1) {
+        this.telefono1 = telefono1;
+    }
+    public String getTelefono2() {
+        return telefono2;
+    }
+    public void setTelefono2(String telefono2) {
+        this.telefono2 = telefono2;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public Integer getEstado() {
+        return estado;
+    }
+    public void setEstado(Integer estado) {
+        this.estado = estado;
+    }
+    @Override
+    public String toString() {
+        return "Clientes [id=" + id + ", nombre=" + nombre + ", correo=" + correo + ", zip=" + zip + ", telefono1="
+                + telefono1 + ", telefono2=" + telefono2 + ", pais=" + pais + ", direccion=" + direccion + ", estado="
+                + estado + ", getId()=" + getId() + ", getNombre()=" + getNombre() + ", getCorreo()=" + getCorreo()
+                + ", getZip()=" + getZip() + ", getTelefono1()=" + getTelefono1() + ", getTelefono2()=" + getTelefono2()
+                + ", getPais()=" + getPais() + ", getDireccion()=" + getDireccion() + ", getEstado()=" + getEstado()
+                + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+                + "]";
+    }
+
+    
+}
